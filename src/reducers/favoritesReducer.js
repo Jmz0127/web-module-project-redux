55d4ad9984685530f623e8e8ecab13987ef3ce13
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-09 18:49:08 -0500

    added three favorites actions and implemented the hideFavorites action. two more actions to implement

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,8 +1,11 @@
+import { REMOVE_FAVORITE, TOGGLE_FAVORITES, ADD_FAVORITE } from '../actions/favoritesActions';
+import { ADD_MOVIE } from '../actions/movieActions';
+
 //set up initialState
 const initialState = {
 	favorites: [
 		{ title: 'Movie', id: 1 },
-		{ title: 'Movie', id: 1 }
+		{ title: 'Movie', id: 2 }
 	],
 	displayFavorites: true
 };
@@ -14,6 +17,30 @@ const initialState = {
 
 const favoritesReducer = (state = initialState, action) => {
 	switch (action.type) {
+		//Add in reducer cases, action creators and event handler code for the following actions:
+
+		//toggleFavorites : Switches the displayFavorites state value between true and false. When displayFavorites is true, the favorite button does not show on the Movie page.
+		case TOGGLE_FAVORITES: {
+			return {
+				...state,
+				displayFavorites: !state.displayFavorites
+			};
+		}
+		//addFavorites: Adds in a new movie object into the favorites list.
+		case ADD_FAVORITE: {
+			return {
+				...state,
+				favorites: [...state.favorites, action.payload]
+			};
+		}
+
+		//removeFavorite: Removes a movie Object from the favorites list with an id passed in.
+		case REMOVE_FAVORITE: {
+			return {
+				...state,
+				favorites: state.filter((item) => item.id !== action.payload)
+			};
+		}
 		default:
 			return state;
 	}
