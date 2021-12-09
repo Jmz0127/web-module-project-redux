f69f9aeb91f603678a115cb0e1996cff316361e9
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-09 18:54:29 -0500

    favorite movies sidebar disappears when you hit the blue hide favorites button in the header as well

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import MovieList from './MovieList';
@@ -11,41 +11,46 @@ import MovieHeader from './MovieHeader';
 import AddMovieForm from './AddMovieForm';
 import FavoriteMovieList from './FavoriteMovieList';
 
-const App = props => {
-  const displayFavorites = true;
+const App = (props) => {
+	const { displayFavorites } = props;
 
-  return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" >Redux Module Project</span>
-      </nav>
+	return (
+		<div>
+			<nav className='navbar navbar-dark bg-dark'>
+				<span className='navbar-brand'>Redux Module Project</span>
+			</nav>
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          {displayFavorites && <FavoriteMovieList/>}
-        
-          <Switch>
-            <Route exact path="/movies/add">
-              <AddMovieForm />
-            </Route>
+			<div className='container'>
+				<MovieHeader />
+				<div className='row '>
+					{displayFavorites && <FavoriteMovieList />}
 
-            <Route path="/movies/:id">
-              <Movie />
-            </Route>
+					<Switch>
+						<Route exact path='/movies/add'>
+							<AddMovieForm />
+						</Route>
 
-            <Route path="/movies">
-              <MovieList/>
-            </Route>
+						<Route path='/movies/:id'>
+							<Movie />
+						</Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </div>
-  );
+						<Route path='/movies'>
+							<MovieList />
+						</Route>
+
+						<Route path='/'>
+							<Redirect to='/movies' />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		displayFavorites: state.favoritesReducer.displayFavorites
+	};
+};
+export default connect(mapStateToProps)(App);
