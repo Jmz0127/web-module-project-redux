1d15ae183d1d0b5c64b5f5290a3ce83ba184ec0e
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-09 16:05:56 -0500

    added movieheader to conntect to state

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const MovieHeader = (props) => {
-    const appTitle = "";
-    const displayFavorites = true;
-    
-    return(<div className="table-title">
-        <div className="row">
-        <div className="col-sm-6">
-            <h2>{appTitle}</h2>
-        </div>
-        <div className="col-sm-6 headerBar">
-            <div className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
-            <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
-            <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
-        </div>
-        </div>
-    </div>);
-}
+	const { appTitle } = props;
+	const displayFavorites = true;
 
-export default MovieHeader;
+	return (
+		<div className='table-title'>
+			<div className='row'>
+				<div className='col-sm-6'>
+					<h2>{appTitle}</h2>
+				</div>
+				<div className='col-sm-6 headerBar'>
+					<div className='btn btn-sm btn-primary'>
+						<span>{displayFavorites ? 'Hide' : 'Show'} Favorites</span>
+					</div>
+					<Link to='/movies' className='btn btn-sm btn-primary'>
+						View All Movies
+					</Link>
+					<Link to='/movies/add' className='btn btn-sm btn-success'>
+						<i className='material-icons'>&#xE147;</i> <span>Add New Movie</span>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+const mapStateToProps = (state) => {
+	//Finally, MovieHeader uses appTitle to display the title text. Connect this component to appTitle and test appTitle is correctly displayed in your app.
+	return {
+		appTitle: state.appTitle
+	};
+};
+
+export default connect(mapStateToProps)(MovieHeader);
