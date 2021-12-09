9d30834a100963f4fc6e05c47fa04990dbf1b6bb
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-09 18:59:51 -0500

    added add favorite to favorite sidebar functionality

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,15 +3,19 @@ import { connect } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 
 import { deleteMovie } from '../actions/movieActions';
+import { addFavorite } from '../actions/favoritesActions';
 const Movie = (props) => {
 	const { id } = useParams();
 	const { push } = useHistory();
 
-	const { movies, displayFavorites, deleteMovie } = props;
+	const { movies, displayFavorites, deleteMovie, addFavorite } = props;
 	const movie = movies.find((movie) => movie.id === Number(id));
 	const handleDeleteClick = () => {
 		deleteMovie(movie.id);
 		push('/movies');
+	};
+	const handleFavorite = () => {
+		addFavorite(movie);
 	};
 
 	return (
@@ -53,7 +57,11 @@ const Movie = (props) => {
 							</section>
 
 							<section>
-								{displayFavorites && <span className='m-2 btn btn-dark'>Favorite</span>}
+								{displayFavorites && (
+									<span onClick={handleFavorite} className='m-2 btn btn-dark'>
+										Favorite
+									</span>
+								)}
 								<span className='delete' onClick={handleDeleteClick}>
 									{' '}
 									{/*Find the HTML element that should trigger a deletion in the movie component. Create and connect the necessary event handlers to call deleteMovie on the current movie's id. After setting the state, redirect the user using the push('/movies') command. */}
@@ -75,4 +83,4 @@ const mapStateToProps = (state) => {
 		movies: state.movieReducer.movies
 	}; //Notice that your movie functions no longer work. Why? Make changes necessary to get the component connected to the movie reducer working again.
 };
-export default connect(mapStateToProps, { deleteMovie })(Movie); // We can delete movies within the Movie Component. Connect the deleteMovie action through the connect method.
+export default connect(mapStateToProps, { deleteMovie, addFavorite })(Movie); // We can delete movies within the Movie Component. Connect the deleteMovie action through the connect method.
